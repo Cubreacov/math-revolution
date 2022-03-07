d4ffd3d50918d7d95f1ce13f6f9f64e047584879
Daniela Cubreacov <1daniela.cubreacov@gmail.com>
2022-03-06 23:23:37 -0800

    more changes

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const cards = document.querySelectorAll(".card");
 
+let cardOne, cardTwo;
 function flipCard(e) {
     let clickedCard = e.target;
-    clickedCard.classList.add("flip");
+    if(clickedCard !== cardOne) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
 
+        let cardOneImg = cardOne.querySelector("img").src,
+        cardTwoImg = cardTwo.querySelector("img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2){
+   if(img1 === img2) {
+       return console.log("Card matched")
+   }
+   setTimeout(() =>{
+   cardOne.classList.add("shake");
+   cardTwo.classList.add("shake");
+}, 400);
+
+    setTimeout(() =>{
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+ }, 1200);
 }
 
 cards.forEach(card =>{
